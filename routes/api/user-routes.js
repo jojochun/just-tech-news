9b78f164ecbf739ab8803ba7ccb454f4acea14d8
jochun <jkchun@protonmail.com>
2022-01-25 12:35:05 -1000

    verify password

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -55,6 +55,32 @@ router.post('/', (req, res) => { // expects {username: 'Lernantino', email: 'ler
 
 });
 
+// This route will be found at http://localhost:3001/api/users/login in the browser
+router.post('/login', (req, res) => {
+    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+        }
+
+        res.json({ user: dbUserData });
+
+        // Verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+});
+
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
 
